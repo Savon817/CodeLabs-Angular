12a9a8d2f116aee9b109e151503fb2117c5912f7
Savon Williams <44207109+Savon817@users.noreply.github.com>
2022-09-26 20:10:13 -0500

    Worked on exercise 4

--- class-12/class-12-exercises/src/app/app.component.ts
+++ class-12/class-12-exercises/src/app/app.component.ts
@@ -6,24 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 
-
 export class AppComponent {
-  title = 'class-12-exercises';
-  width: number = 200;
-  height: number = 500;
-  isDisabled:Boolean = true;
-
-  constructor(){
-    setTimeout(() => {
-      this.width = 500
-    }, 1000)
-  }
-
-  logConsole(){
-    console.log("Hello there!")
-  }
-
-  toggleDisable(){
-    this.isDisabled= !this.isDisabled;
-  }
+  title:string="Header"
 }
