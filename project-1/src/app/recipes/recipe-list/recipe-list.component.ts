5e933e82034160b84345bb32aa6acd1ac9f31d04
Savon817 <44207109+Savon817@users.noreply.github.com>
2022-10-02 15:36:43 -0500

    more worked on course project

--- project-1/src/app/recipes/recipe-list/recipe-list.component.ts
+++ project-1/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,9 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
-    new Recipe('A test recipe', 'This simply a test', 'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2022_38/1909415/marcela-valladolid-makes-flautas-zz-220920-2x1.jpg'),
-    new Recipe('A test recipe', 'This simply a test', 'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2022_38/1909415/marcela-valladolid-makes-flautas-zz-220920-2x1.jpg')
+    new Recipe('A test recipe', 'This is simply a test', 'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2022_38/1909415/marcela-valladolid-makes-flautas-zz-220920-2x1.jpg'),
+    new Recipe('Another test recipe', 'This is simply a test', 'https://media-cldnry.s-nbcnews.com/image/upload/newscms/2022_38/1909415/marcela-valladolid-makes-flautas-zz-220920-2x1.jpg')
   ];
 
   constructor() { }
@@ -17,4 +18,7 @@ export class RecipeListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe);
+  }
 }
