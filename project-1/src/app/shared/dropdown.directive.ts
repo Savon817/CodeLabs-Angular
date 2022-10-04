c1fde1e1a49415f9dc60ef9d6c303fddac20a98b
Savon817 <44207109+Savon817@users.noreply.github.com>
2022-10-04 12:51:03 -0500

    Updated project1, added dropdown directive

---
+++ project-1/src/app/shared/dropdown.directive.ts
@@ -0,0 +1,13 @@
+import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core";
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective{
+  @HostBinding('class.open') isOpen = false;
+  constructor(private elementRef: ElementRef){}
+
+  @HostListener('click') dropDownClick(){
+    this.isOpen = !this.isOpen;
+  }
+}
