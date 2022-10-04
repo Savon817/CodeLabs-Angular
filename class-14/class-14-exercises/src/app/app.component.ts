e68708e2b49689c2ca4576d45d700ffa041d3918
Savon Williams <44207109+Savon817@users.noreply.github.com>
2022-10-03 20:27:18 -0500

    working on exercise 3

--- class-14/class-14-exercises/src/app/app.component.ts
+++ class-14/class-14-exercises/src/app/app.component.ts
@@ -6,11 +6,20 @@ import { Component, Input, Output } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  myPosts: string[] = [
-    "Hello",
-    "There",
-    "My",
-    "Young",
-    "Padawan."
-  ];
+  // myPosts: string[] = [
+  //   "Hello",
+  //   "There",
+  //   "My",
+  //   "Young",
+  //   "Padawan."
+  // ];
+
+  users: {firstName: string, id: number}[] = [
+    {firstName: 'Savon',
+    id: 9409},
+    {firstName: 'Buck',
+    id: 4818},
+    {firstName: 'Sonny',
+    id: 1474}];
+
 }
