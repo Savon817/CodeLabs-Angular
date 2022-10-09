7c21f3a22855326aa3894d7ec2bc578c7bb82e4a
Savon817 <44207109+Savon817@users.noreply.github.com>
2022-10-09 17:38:18 -0500

    Added RecipeEdit, Updated necessary files for routing

--- project-1/src/app/recipes/recipe.service.ts
+++ project-1/src/app/recipes/recipe.service.ts
@@ -30,6 +30,10 @@ export class RecipeService{
     return this.recipes.slice();
   }
 
+  getRecipe(index: number){
+    return this.recipes[index];
+  }
+
   addIngredientToShoppingList(ingredient: Ingredient[]){
     this.slService.addIngredients(ingredient);
   }
