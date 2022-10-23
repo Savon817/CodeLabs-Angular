f6d34833f7cd2a72602f4f9cd7a7769eb9a057d2
Savon817 <44207109+Savon817@users.noreply.github.com>
2022-10-22 19:58:46 -0500

    Added Http Requests

--- project-1/src/app/recipes/recipe.service.ts
+++ project-1/src/app/recipes/recipe.service.ts
@@ -10,23 +10,28 @@ import { Recipe } from "./recipe.model";
 export class RecipeService{
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe('Delicious Tacos', 'Crazy Good Tacos', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/birria-tacos-1648842127.jpg',
-    [
-      new Ingredient('Meat', 1),
-      new Ingredient('Tortillas', 3),
-      new Ingredient('Sauce', 1)
-    ]),
-    new Recipe("Tasty Shrimp Po'Boy", 'A recipe that cannot go wrong', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/08/20/0/FNM_100120-Shrimp-Po-Boy_s4x3.jpg.rend.hgtvcom.616.462.suffix/1597931264750.jpeg',
-    [
-      new Ingredient('Shrimp', 15),
-      new Ingredient('Bread', 2),
-      new Ingredient('Tomatoes', 1),
-      new Ingredient('Sauce', 1)
-    ])
-  ];
-
+  // private recipes: Recipe[] = [
+  //   new Recipe('Delicious Tacos', 'Crazy Good Tacos', 'https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/birria-tacos-1648842127.jpg',
+  //   [
+  //     new Ingredient('Meat', 1),
+  //     new Ingredient('Tortillas', 3),
+  //     new Ingredient('Sauce', 1)
+  //   ]),
+  //   new Recipe("Tasty Shrimp Po'Boy", 'A recipe that cannot go wrong', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2020/08/20/0/FNM_100120-Shrimp-Po-Boy_s4x3.jpg.rend.hgtvcom.616.462.suffix/1597931264750.jpeg',
+  //   [
+  //     new Ingredient('Shrimp', 15),
+  //     new Ingredient('Bread', 2),
+  //     new Ingredient('Tomatoes', 1),
+  //     new Ingredient('Sauce', 1)
+  //   ])
+  // ];
+  private recipes: Recipe[] = [];
   constructor(private slService: ShoppingListService) { }
+
+  setRecipes(recipes: Recipe[]){
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
 
   getRecipes(){
     return this.recipes.slice();
