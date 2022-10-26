43071c5e8d71cea1c25515daa626ee58e573ba84
Savon817 <44207109+Savon817@users.noreply.github.com>
2022-10-26 16:54:47 -0500

    added authentication

---
+++ project-1/src/app/auth/auth.service.ts
@@ -0,0 +1,102 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { catchError, tap } from "rxjs/operators";
+import { Subject, throwError } from "rxjs";
+import { User } from "./user.model";
+
+export interface AuthResponseData {
+  kind?: string;
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localID: string;
+  registered?: boolean;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  user = new Subject<User>()
+  constructor(private http: HttpClient){}
+
+
+  signUp(email: string, password: string){
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyA-CmyS9tz72pduDK_WjeA6jbpIGsh_hYI',
+      {
+        email: email,
+        password: password,
+        returnSecureToken: true
+      }
+    ).pipe(
+        catchError(this.handleError),
+        tap(resData => {
+          this.handleAuthentication(
+            resData.email,
+            resData.localID,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  login(email: string, password: string){
+    return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyA-CmyS9tz72pduDK_WjeA6jbpIGsh_hYI',
+      {
+          email: email,
+          password: password,
+          returnSecureToken: true
+      }
+    ).pipe(
+      catchError(this.handleError),
+      tap(resData => {
+        this.handleAuthentication(
+          resData.email,
+          resData.localID,
+          resData.idToken,
+          +resData.expiresIn
+        );
+      })
+    );
+  }
+
+  private handleAuthentication(
+    email: string,
+    userId: string,
+    token:string,
+    expiresIn: number){
+    resData => {
+      let expirationDate = new Date(
+        new Date().getTime() + expiresIn * 1000
+      );
+      const user = new User(
+        email,
+        userId,
+        token,
+        expirationDate
+      );
+      this.user.next(user);
+    }
+  }
+
+  private handleError(errorRes: HttpErrorResponse){
+    let errorMessage = 'An unknown error occurred'
+    if(!errorRes.error || !errorRes.error.error){
+      return throwError(errorMessage);
+    }
+    switch(errorRes.error.error.message){
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email already exists';
+        break;
+      case 'EMAIL_NOT_FOUND':
+        errorMessage = 'This email does not exist';
+        break;
+      case 'INVALID_PASSWORD':
+        errorMessage = 'This password is not correct';
+        break;
+    }
+    return throwError(errorMessage);
+    }
+}
